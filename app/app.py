154483eba68765b0d601bd020148aaea3b629b59
zaid kamil <xaidmetamorphos@gmail.com>
2020-06-23 04:30:00 +0530

    basic app

--- app/app.py
+++ app/app.py
@@ -1,1 +1,24 @@
 import streamlit as st
+import scraper as sc
+
+# variables
+soup = None
+
+st.sidebar.title("Menu options")
+language = st.sidebar.selectbox("select a language",['python','javascript'])
+date_range = st.sidebar.selectbox("date range",['daily','weekly','monthly'])
+
+url =  sc.get_dynamic_url(language,date_range)
+st.title("Data mining - live")
+st.text('data will be scraped from')
+st.text_input('the url to be scraped',value=url)
+if st.button('start mining'):
+    with st.spinner('collecting data from url'):
+        soup = sc.get_data_as_soup(url)
+        st.success("webpage loaded")
+
+if soup :
+    view_raw_data = st.checkbox('view raw data')
+    if view_raw_data:
+        raw_data = sc.view_raw_data(soup)
+        st.write(raw_data)
--- app/scraper.py
+++ app/scraper.py
@@ -2,3 +2,15 @@ import requests
 from bs4 import BeautifulSoup 
 import pandas as pd
 
+def get_dynamic_url(base_url='https://github.com/trending',language="python",date_range="weekly"):
+    scrape_url = f"https://github.com/trending/{language}?since={date_range}&spoken_language_code=en"
+    return scrape_url
+
+def get_data_as_soup(url):
+    page = requests.get(url)
+    if page.status_code == 200:
+        return BeautifulSoup(page.text, 'html5lib')
+
+def view_raw_data(soup):
+    return soup.text
+    
