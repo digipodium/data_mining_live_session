0b04b74c3aadb2ad91112e6b8bea03687e7b2300
zaid kamil <xaidmetamorphos@gmail.com>
2020-06-23 07:37:19 +0530

    finised

--- app/app.py
+++ app/app.py
@@ -1,24 +1,55 @@
 import streamlit as st
 import scraper as sc
-
+import os
+import pandas as pd
 # variables
 
 st.sidebar.title("Menu options")
-language = st.sidebar.selectbox("select a language",['python','javascript'])
-date_range = st.sidebar.selectbox("date range",['daily','weekly','monthly'])
+lang = st.sidebar.selectbox("select a language",['python','javascript'])
+date = st.sidebar.selectbox("date range",['daily','weekly','monthly'])
 
-url =  sc.get_dynamic_url(language,date_range)
+# step 1
+url =  sc.get_dynamic_url(language=lang,date_range=date)
 st.title("Data mining - live")
 st.text('data will be scraped from')
 st.text_input('the url to be scraped',value=url)
-view_raw_data = st.checkbox('view raw data')
+st.write('select options ')
+view_raw_data = st.checkbox('view raw data (not the best idea)')
+view_dictionary_data = st.checkbox('view data dictionary')
+save_data = st.checkbox('save data to file')
 if st.button('start mining'):
     with st.spinner('collecting data from url'):
         soup = sc.get_data_as_soup(url)
         st.success("webpage loaded")
 else:
     soup = None 
+# step 2
 if soup :
     if view_raw_data:
-        raw_data = sc.view_raw_data(soup)
-        st.markdown(raw_data)
+        with st.spinner('loading raw data'):
+            raw_data = sc.view_raw_data(soup)
+            st.markdown(raw_data)
+    rows = sc.get_rows(soup)
+    datadict = sc.extract_data(rows,date)
+    if view_dictionary_data:
+        with st.spinner('loading data dictionary'):
+            st.write(datadict)
+    if save_data:
+        with st.spinner('saving data as dataset'):
+            df = sc.save_data(datadict,lang,date)
+            st.success('data saved')
+            st.write(df)
+
+files = os.listdir('datasets')
+if files:
+    selected_files = st.sidebar.selectbox('created datasets',files)
+btnview = st.sidebar.button('view')
+if btnview:
+    content = pd.read_csv('datasets/'+selected_files)
+    st.subheader("viewing saved dataset")
+    st.write(content)
+
+btngraph = st.sidebar.button('view graph')
+if btngraph:
+    fig = sc.generate_graph('datasets/'+selected_files)
+    st.plotly_chart(fig)
--- app/scraper.py
+++ app/scraper.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup 
 import pandas as pd
+from datetime import date
+import plotly.express as px
 
 def get_dynamic_url(base_url='https://github.com/trending',language="python",date_range="weekly"):
     scrape_url = f"https://github.com/trending/{language}?since={date_range}&spoken_language_code=en"
@@ -14,3 +16,47 @@ def get_data_as_soup(url):
 def view_raw_data(soup):
     return soup
     
+def get_rows(soup):
+    rows = []
+    tag = 'article'
+    tagclass = "Box-row"
+    rows = soup.find_all(tag,attrs={'class':tagclass})
+    return rows
+
+def extract_data(rows,date):
+    datadict = []
+    for item in rows :
+        username, reponame = item.find('h1').text.replace(' ','').split()
+
+        try:description = item.find('p').text 
+        except: description = ""
+
+        link = "https://github.com/" + item.find('h1').find('a').attrs.get('href')
+        language = item.find('span',attrs={'itemprop':'programmingLanguage'}).text
+        starsTag,forkTag = item.find_all('a',attrs={'class':'muted-link'})
+        stars = starsTag.text
+        forks = forkTag.text
+        datadict.append({
+            'username': username,
+            'repository':reponame,
+            'link' : link,
+            'total_stars':stars,
+            'total_forks':forks,
+            'language':language,
+            'range':date,
+            'description':description
+        })
+    return datadict
+
+def save_data(datadict,lang,range):
+    df = pd.DataFrame(datadict)
+    df.to_csv('datasets/'+lang+'_'+range+'_'+str(date.today()),index=False)
+    return df
+
+def generate_graph(filepath):
+    df = pd.read_csv(filepath)
+    df.total_stars = df.total_stars.apply(lambda val :int(val.strip().replace(',','')))
+    df.total_forks = df.total_forks.apply(lambda val :int(val.strip().replace(',','')))
+    fig = px.scatter_3d(df,x='repository',y='total_stars',z='total_forks',title=filepath,size='total_stars',color='total_forks')
+    # fig = px.bar(df,x='repository',y='total_stars',title=filepath)
+    return fig
